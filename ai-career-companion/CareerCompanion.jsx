7f3358f8a69c9a01856bc88b544ec9b5acb5c3c8
paybailey <aicareercompanion@gmail.com>
2025-05-31 18:46:01 -0500

    Add files via upload

---
+++ ai-career-companion/CareerCompanion.jsx
@@ -0,0 +1,1 @@
+<paste React component code here>
---
+++ ai-career-companion/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CareerCompanion from "./CareerCompanion.jsx";
+
+ReactDOM.render(<CareerCompanion />, document.getElementById("root"));
